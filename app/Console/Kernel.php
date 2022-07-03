0ad8bf06c88ff1b2fe742b7f64b09d49e2039202
Dan Brown <ssddanbrown@googlemail.com>
2022-07-03 14:17:26 +0100

    Modified feed updating scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateFeedsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('rss:update-outdated-feeds')->hourly();
+        $schedule->command(UpdateFeedsCommand::class, ['--outdated-time=0'])->hourly();
     }
 
     /**
