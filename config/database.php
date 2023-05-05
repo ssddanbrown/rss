6531f238fb4c08e443b5259357401e40198b76fc
Shift <shift@laravelshift.com>
2023-05-05 20:32:06 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => 'sqlite',
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -37,10 +37,10 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            //'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', storage_path('database/database.sqlite')),
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'foreign_key_constraints' => true,
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
